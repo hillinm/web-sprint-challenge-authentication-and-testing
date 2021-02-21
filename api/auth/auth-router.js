959c0202373ea3846ae54ec5ff56f5a12e6afeff
Mark Hillin <mark.hillin@gmail.com>
2021-02-21 17:30:03 -0600

    18 passing

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const secrets = require('../config/secrets.js');
+const { isValid } = require("../users/users-service.js");
 
 const Users = require('../users/users-model.js');
 
@@ -31,35 +32,41 @@ router.post('/register', userCheck, (req, res) => {
   })
 });
 
-router.post('./login', (req, res) => {
+router.post('/login', async (req, res, next) => {
   const { username, password } = req.body;
 
-  Users.findBy({ username })
-      .first()
-      .then(([user]) => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          const token = generateToken(user)
-          res.status(200).json({ message: `welcome ${user.username}`, token});
-        } else {
-          res.status(401).json({ message: 'invalid credentials'});
-        }
-      })
-      .catch(err => {
-        res.status(500).json({ message: 'username and password required', err });
-      });
-  });
-
-
-  function generateToken(user) {
-    const payload = {
-        userId: user.id,
-        username: user.username
+  try {
+    if (!isValid(req.body)) {
+      next({ apiCode: 400, apiMessage: 'invalid credentials' })
+    } else {
+      const [user] = await Users.findBy({ username: username });
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: `welcome, ${user.username}`, token })
+      } else {
+        next({ apiCode: 401, apiMessage: 'username and password required' });
+      }
     }
-    const options = {
-        expiresIn: "1hr"
-    }
-    const secret = secrets.jwtSecret
-    return jwt.sign(payload, options, secret);
+  } catch (err) {
+    next({apiCode: 500, apiMessage: 'invalid credentials'})
+  }
+});
+
+function generateToken(user) {
+
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  const token = jwt.sign(payload, secrets.jwtSecret, options);
+
+  return token;
+
 }
 
 module.exports = router;
